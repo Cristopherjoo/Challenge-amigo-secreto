269c896cb093ec8ed31501be5765b63597f47978
Cristopherjoo <cristopherjoo@gmail.com>
2025-02-25 04:09:44 +0000

    sortear amigos

--- app.js
+++ app.js
@@ -4,3 +4,25 @@ let amigos = [];
 let inputAmigo = document.getElementById("amigo");
 let listaAmigos = document.getElementById("listaAmigos");
 let resultado = document.getElementById("resultado");
+
+
+function agregarAmigo() {
+    let nombre = inputAmigo.value.trim();
+    
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre.");
+        return;
+    }
+  
+    amigos.push(nombre);
+
+    inputAmigo.value = "";
+
+    listaAmigos.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let amigo = amigos[i];
+        let li = document.createElement("li");
+        li.textContent = amigo;
+        listaAmigos.appendChild(li);
+    }
+}
